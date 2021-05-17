3e283815b5709e173ac081f92fb95ebfa8894052
mauser7x63 <83775990+mauser7x63@users.noreply.github.com>
2021-05-17 21:33:06 +0500

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class Post(BaseModel):
+    id: int
+    content: str
+    author_id: int
+    likes: int = 0
+    dislikes: int = 0
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    class Config:
+        orm_mode = True
+
+class AuthModel(BaseModel):
+    username: str
+    password: str
