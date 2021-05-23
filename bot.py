a94c2453235828e37d591561d450c1df7b12e0df
Mauser <mauser7x63@yandex.ru>
2021-05-23 20:15:29 +0500

    load bots

--- bot.py
+++ bot.py
@@ -4,19 +4,48 @@ from faker import Faker
 from requests.api import post
 
 fake = Faker()
-Faker.seed(0)
-endpoint = 'http://127.0.0.1:9000/'
+Faker.seed(3)
+endpoint = 'http://127.0.0.1:8000/'
 headers = {
             'accept': 'application/json',
             'Content-Type': 'application/json'
         }
 
-def loadConfig():
-    with open('bots_config.json') as f:
+def loadFromJSNON(fileName):
+    '''
+    загрузить из JSON. Принимает строку с именем файла 
+    '''
+    with open(fileName) as f:
         fileStuff = f.read()
-        configJson = json.loads(fileStuff)
-    return configJson
+        loadedStructure = json.loads(fileStuff)
+    return loadedStructure
+   
+def saveToJSON(fileName, data):
+    '''
+    сохранить структуру в JSON-формате. Принимает строку с именем файла и структуру для сохранения
+    '''
+    with open(fileName, 'w') as outfile:
+        json.dump(data, outfile, indent=4, sort_keys=True)
+    return
 
+def loadBots():
+    bots = loadFromJSNON('fastAPI_socnet/bots.json')
+    if rules['number_of_users']>len(bots):
+        for user in range(0, rules['number_of_users']-len(bots)):
+            bot = Bot(name=fake.first_name_nonbinary(), passwd=fake.pystr())
+            print(f'user: {bot.username} with passwd: {bot.password}')
+            bots.update({
+                bot.username: bot.password
+            })
+        saveToJSON(fileName='fastAPI_socnet/bots.json', data=bots)
+        print(len(bots), ' bots has been saved')
+    return bots
+
+def prepearBots(botsDict):
+    for user in botsDict:
+        user = Bot(name=user, passwd=botsDict[user])
+        print(user.username)
+    
 
 class Bot():
     def __init__(self, name, passwd):
@@ -58,7 +87,8 @@ class Bot():
             return res.status_code     
     
     def createPost(self, user_id, text):
-        url = f'{endpoint}/newpost/?token={self.token}'
+        url = f'{endpoint}newpost/?token={self.token}'
+        print(url)
         body = {
             "content": text,
             "user_id": user_id,
@@ -89,19 +119,13 @@ class Bot():
 if __name__=="__main__":
     from pprint import pprint
     print('bot standalone runned')
-    rules = loadConfig()
+    rules = loadFromJSNON('fastAPI_socnet/bots_config.json')
     pprint(rules)
     
-    for _ in range(rules['number_of_users']):
+    #for _ in range(rules['number_of_users']):
         #bot = Bot(name=fake.first_name_nonbinary(), passwd=fake.pystr())
         #print(f'user: {bot.username} with passwd: {bot.password}')
-        print(fake.paragraph(nb_sentences=3))
-        print('*'*20)
-
-        
-    
-    bot = Bot(name=fake.first_name_nonbinary(), passwd=fake.pystr())
-    print(f'user: {bot.username} with passwd: {bot.password}')
-    #print(fake.paragraph(nb_sentences=3))
-    print(bot.token)
-    print(bot.createPost())
+        #print(fake.paragraph(nb_sentences=3))
+        #print('*'*20)
+users = loadBots()
+prepearBots(users)
